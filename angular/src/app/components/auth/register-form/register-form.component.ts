a4e8a109de7015fe1188fb4404dfeab0e9632d29
NAbrain <michal2002.60@gmail.com>
2024-10-05 11:40:43 +0200

    add login-form, register-form, page-not-found components

---
+++ angular/src/app/components/auth/register-form/register-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/AuthService';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-form.component.html',
+  styleUrl: './register-form.component.css'
+})
+export class RegisterFormComponent {
+
+}
