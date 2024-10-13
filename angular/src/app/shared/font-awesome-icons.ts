46e4de40d8a6fc09f3380e5ff778c7343de8b876
NAbrain <michal2002.60@gmail.com>
2024-10-13 17:20:46 +0200

    icons

---
+++ angular/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,5 @@
+import { faBook, faBookOpen, faComments, faGear, faRightFromBracket, IconDefinition } from "@fortawesome/free-solid-svg-icons"
+
+export const fontAwesomeIcons: IconDefinition[] = [
+    faGear, faRightFromBracket, faBookOpen, faBook, faComments
+]
