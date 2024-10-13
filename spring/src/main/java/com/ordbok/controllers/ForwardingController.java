2a2c456da4c34fab742321919b1e4216d5d2d61c
NAbrain <michal2002.60@gmail.com>
2024-10-13 17:13:54 +0200

    comment out for now

---
+++ spring/src/main/java/com/ordbok/controllers/ForwardingController.java
@@ -0,0 +1,14 @@
+package com.ordbok.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ForwardingController {
+
+//    @GetMapping({"/", "/login", "/register"})
+//    public String forward() {
+//        return "forward:/index.html";
+//    }
+}
